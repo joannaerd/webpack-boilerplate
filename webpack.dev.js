f4a45d481732858ae0583bf77685646d2ee7e3ad
Joanna Erd <joanna.erd@elpassion.pl>
2018-07-04 16:56:30 +0200

    Add stylelint

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,15 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const StyleLintPlugin = require('stylelint-webpack-plugin');
+
 module.exports = merge(common, {
   mode: 'development',
   devServer: {
     contentBase: '/dist/',
     publicPath: '/',
-  }
+  },
+  plugins: [
+    new StyleLintPlugin({}),
+  ],
 });
