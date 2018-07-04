e23916873251906e2c708985df5134fe635018c5
Joanna Erd <joanna.erd@elpassion.pl>
2018-07-04 17:34:15 +0200

    Add eslint

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const path = require('path');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = merge(common, {
@@ -8,6 +9,16 @@ module.exports = merge(common, {
   devServer: {
     contentBase: '/dist/',
     publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/js'),
+        loader: 'eslint-loader',
+      },
+    ],
   },
   plugins: [
     new StyleLintPlugin({}),
